4867f3d9f97ee3d306453da6cfee0b3e4bf62849
aleena6875 <aleenashibu48@gmail.com>
2023-09-11 19:45:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/datemonth/SecondActivity.kt
@@ -0,0 +1,44 @@
+package com.example.datemonth
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigationrail.NavigationRailView
+
+class SecondActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val navRailFab: FloatingActionButton = findViewById(R.id.nav_rail_fab)
+        navRailFab.setOnClickListener {
+            Toast.makeText(this, "FAB Clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        val navigationRail: NavigationRailView = findViewById(R.id.navigationRail)
+        navigationRail.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.inbox -> {
+                    Toast.makeText(this, "Inbox", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.primary -> {
+                    Toast.makeText(this, "Primary", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.social-> {
+                    Toast.makeText(this, "Social", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.starred -> {
+                    Toast.makeText(this, "Starred", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
