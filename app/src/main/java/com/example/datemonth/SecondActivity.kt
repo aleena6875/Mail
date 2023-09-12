5f956acf2e53fc41636367e6356ac062f990357c
aleena6875 <aleenashibu48@gmail.com>
2023-09-12 11:01:13 +0530

    date picker is added

--- app/src/main/java/com/example/datemonth/SecondActivity.kt
+++ app/src/main/java/com/example/datemonth/SecondActivity.kt
@@ -2,12 +2,16 @@ package com.example.datemonth
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigationrail.NavigationRailView
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var getdate: Button
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,20 +29,32 @@ class SecondActivity : AppCompatActivity() {
                     Toast.makeText(this, "Inbox", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 R.id.primary -> {
                     Toast.makeText(this, "Primary", Toast.LENGTH_SHORT).show()
                     true
                 }
-                R.id.social-> {
+
+                R.id.social -> {
                     Toast.makeText(this, "Social", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 R.id.starred -> {
                     Toast.makeText(this, "Starred", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 else -> false
             }
+        }
+        getdate = findViewById(R.id.getdate)
+        getdate.setOnClickListener {
+            val dataPicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+            dataPicker.show(supportFragmentManager, dataPicker.toString())
         }
     }
 }
